653d014ddaa11dcf9568d3ebc049c4dd5a593817
Pouya Arvandian <pouya.arvandian@kalido.com.au>
2018-06-25 11:46:38 +1000

    Implemented this further.

--- count_words.py
+++ count_words.py
@@ -2,9 +2,13 @@
 
 import click
 import re
+from collections import Counter
+from operator import attrgetter
+from prettytable import PrettyTable
 
 
-word_pattern = re.compile(r"[\w']+")
+#word_pattern = re.compile(r"[\w']+")
+word_pattern = re.compile(r"\d*[a-zA-Z'][a-zA-Z'\d]*")
 
 def words_in_text(s):        
     return word_pattern.findall(s)
@@ -23,59 +27,76 @@ class Words():
         self.additional_conditions = []
 
     def from_file(self,filename):
-        with open(filename, 'rb') as f:
-            content = f.read()
-        # self.words = [Word(word, freq???) for word in words_in_text(content)]
+        with open(filename, 'r') as f:
+            content = f.read().replace('\n', '')
+        counts = Counter([detected_word.upper() for detected_word in words_in_text(content)])
+        self.words = [Word(word, counts[word]) for word in counts]
 
     def min(self, min):
-        self.additional_conditions.append('word.count > {min}'.format(min=min))
+        self.additional_conditions.append('word.frequency >= {min}'.format(min=min))
 
     def exclude(self, exclude_list):
-        self.additional_conditions.append('word not in {exclude_list}'.format(exclude_list=exclude_list))
+        self.additional_conditions.append('word.content not in {exclude_list}'.format(exclude_list=exclude_list))
 
-    def fetch(self):
-        stmt = '[{q} {cond}]'.format(q=self.fetch_query)
-        if len(additional_conditions) > 0:
-            cond = 'if {}'.format(' and '.join(self.additional_conditions))
-            stmt.format(cond=cond)
+    def fetch(self, order='desc'):
+        assert order in ['desc', 'asc'], "Sort order has to be one of 'desc' or 'asc'."
+
+        stmt = '[{query} {conditions}]'
+        if len(self.additional_conditions) > 0:
+            conditions = 'if {}'.format(' and '.join(self.additional_conditions))
             self.additional_conditions = [] #reset
-        return eval(stmt)
-
-    def display_report(self, format='table'):
-        pass
+        else:
+            conditions = ''
+        stmt = stmt.format(query=self.fetch_query, conditions=conditions)
+        reverse = True if order == 'desc' else False
+        return sorted(eval(stmt), key=attrgetter('frequency'), reverse=reverse)
 
 
 def validate_minimum(ctx, param, value):
-    # import ipdb;ipdb.set_trace()
     if value is None:
         return
     if value <= 0:
         raise click.BadParameter('Should be a positive integer value.')
     return value
 
+
 def validate_exclude_list(ctx, param, value):
-    # import ipdb;ipdb.set_trace()
     if value is None:
         return
-    excludes = [i.strip() for i in value.strip().split(',')]
+    excludes = [i.strip().upper() for i in value.strip().split(',')]
     excludes = [i for i in excludes if i != ''] # forgive empty values, extra commas, etc
     if not all(len(words_in_text(exclude)) == 1 for exclude in excludes):
         raise click.BadParameter('Should be a Comma-separated list of words only.')            
     return excludes
 
 
+def print_report(rows):
+    """ Pretty print the words and their frequencies in a table """
+    report = PrettyTable()
+    report.field_names = ["Word", "Frequency"]
+    for row in rows:
+        report.add_row([row.content, row.frequency])
+    print(report)
+
+
 @click.command()
 @click.argument('filename')
-@click.option('--minimum', default=None, type=int, callback=validate_minimum, help='minimum count of words.')
-@click.option('--exclude', default=None, type=str, callback=validate_exclude_list, help='Comma-separated list of words to be excluded from the report.')
-def count_words(filename, minimum, exclude):
+@click.option('--minimum', default=None, type=int, callback=validate_minimum, 
+    help='minimum count of words.')
+@click.option('--exclude', default=None, type=str, callback=validate_exclude_list, 
+    help="""Comma-separated list of words to be excluded from the report.
+    If keyword contains apostrophe character, please escape it. Example: let\\'s.""")
+@click.option('--order', default='desc', type=str, 
+    help="Sort order for word frequencies. 'desc' or 'asc'. Defaults to 'desc'.")
+def count_words(filename, minimum, exclude, order):
     """ Main click command for counting words in a file. """
-    words = Words().from_file(filename)
-    if minimum:
-        words.min(minimum)
-    if exclude:
-        words.exclude(exclude)
-    words.fetch().display_report()
+    words = Words()
+    words.from_file(filename)
+    if minimum: words.min(minimum)
+    if exclude: words.exclude(exclude)
+    rows = words.fetch(order)
+
+    print_report(rows)
 
 
 if __name__ == '__main__':
